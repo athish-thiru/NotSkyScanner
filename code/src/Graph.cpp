21661bd23c3dcf4dfcfbded6af9afd636f94976f
Jasmehar-Kochhar <90488944+Jasmehar-Kochhar@users.noreply.github.com>
2022-11-13 15:45:35 -0600

    Update Graph.cpp

--- code/src/Graph.cpp
+++ code/src/Graph.cpp
@@ -19,21 +19,47 @@ Graph::Graph(){
 //construct the vertices 
 //such that each airport object is connected with its ID
 
-void addEdge(vector <pair<int, int> > adj[], int source_number, int destination_number, int distance)
+void addEdge(vector <pair<srting, int> > adj, int source_number, string destination_number, int distance)
 {
-    adj[source_number].push_back(make_pair(destination_number, distance));
+    adj[source_number].push_back(make_pair(dest_number, distance));
 }
 void addAllEdges(){
 //open file
 //i = 0
 
-    for(i = 0; i<source.size(); i++)
-    int source_number = source[i]; //index of the source airport as in the airport vector
-    int destination_number = destination[i];//index of the destination as in the airport vector
-    int distance = distance[i];//distance between the airports
+    for(i = 0; i<source.size(); i++){
+    int source_number = source[i]; //gives the index of the source airport as in the airport vector (i is the index per line)
+    int dest_number = destination[i];//gives the index of the destination as in the airport vector (i is the index per line)
+    int distance = distance[i];//distance between the airports (i is index per line)
     addEdge(adj, source, destination, distance);
+    }
 }
 
-double distance(int source_number, int destination_number){
+double distance(int source_number, int dest_number){
+    source_lat = airport[source_number].getlat();
+    source_long = airport[source_number].getlon();
+    dest_lat = airport[dest_number].getlat();
+    dest_long = airport[dest_number].getlon();
     
+    source_lat = toRadians(source_lat);
+    source_long = toRadians(source_long);
+    dest_lat = toRadians(dest_lat);
+    dest_long = toRadians(dest_long);
+
+    long double d_long = long2 - long1;
+    long double d_lat = lat2 - lat1;
+ 
+    long double ans = pow(sin(dlat / 2), 2) + cos(lat1) * cos(lat2) * pow(sin(d_long / 2), 2);
+ 
+    ans = 2 * asin(sqrt(ans));
+ 
+    // Radius of Earth in
+    // Kilometers, R = 6371
+    // Use R = 3956 for miles
+    long double R = 6371;
+     
+    // Calculate the result
+    long double distance = ans * R;
+ 
+    return distance;
 }
