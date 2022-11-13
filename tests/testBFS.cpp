e928b3f25c0bf87d1f70ebd416c0e51ef172f10e
Athish Thiruvengadam <athisht2@illinois.edu>
2022-11-13 23:05:43 +0000

    Added test suite

---
+++ tests/testBFS.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp> 
+
+TEST_CASE("Random Test Case 1", "[BFS]") {
+    REQUIRE(1 == 1);
+}
---
+++ tests/testDijkstra.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp> 
+
+TEST_CASE("Random Test Case 2", "[Dijkstra]") {
+    REQUIRE(1 == 1);
+}
