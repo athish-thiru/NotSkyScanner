b6bb06ebcd018a639cd1285b48f9a37474582acd
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-11-13 17:19:50 -0600

    Update airport.h

--- code/src/airport.h
+++ code/src/airport.h
@@ -4,30 +4,31 @@
 using namespace std; 
 
 class Aiport {
-    private:
+    private
 
     string name;
     string city;
     double lat;
     double lon;
     string IATA;
-    double alt; 
+    // double alt; 
 
     public:
-    
+
+    Airport();    
     string getName();
     string getCity();
     double getLat();
     double getLon();
     string getIATA();
-    double getAlt();
+    // double getAlt();
 
     void setName(string input);
     void setCity(string input);
     void setIATA(string input);
     void setLon(double input);
     void setLat(double input);
-    void setAlt(double input);
+    // void setAlt(double input);
 
 
 };
