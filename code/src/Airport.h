acc6608931cc432d88049f46881cda57570ec12d
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-11-13 17:14:25 -0600

    Create Airport.h

---
+++ code/src/Airport.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+
+using namespace std; 
+
+class Aiport {
+    private
+
+    string name;
+    string city;
+    double lat;
+    double lon;
+    string IATA;
+    // double alt; 
+
+    public:
+
+    Airport();    
+    string getName();
+    string getCity();
+    double getLat();
+    double getLon();
+    string getIATA();
+    // double getAlt();
+
+    void setName(string input);
+    void setCity(string input);
+    void setIATA(string input);
+    void setLon(double input);
+    void setLat(double input);
+    // void setAlt(double input);
+
+
+};
