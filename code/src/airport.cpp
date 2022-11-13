afcea1b2e969a654f2b9f959412e3d3052ee36b0
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-11-13 17:20:25 -0600

    Update airport.cpp

--- code/src/airport.cpp
+++ code/src/airport.cpp
@@ -1,41 +1,110 @@
 #include <airport.h>
+#include "vector"
 
 using namespace std;
 
-string getName() {
-    return name;
-}
-string getCity() {
-    return city;
-}
-double getLat() {
-    return lat;
-}
-double getLon() {
-    return lon;
-}
-string getIATA() {
-    return IATA;
-}
-double getAlt() {
-    return alt;
+Airport::Airport() {
+    name = "UNKNOWN";
+    city = "UNKNOWN";
+    IATA = "UNKNOWN";
+    lon = 0;
+    lat = 0;
+
 }
 
-void setName(string input) {
+string Airport:: getName() {
+    return name;
+}
+string Airport::getCity() {
+    return city;
+}
+double Airport::getLat() {
+    return lat;
+}
+double Airport:: getLon() {
+    return lon;
+}
+string Airport::getIATA() {
+    return IATA;
+}
+// double Airport::getAlt() {
+//     return alt;
+// }
+
+void Airport::setName(string input) {
     name = input;
 }
-void setCity(string input) {
+void Airport::setCity(string input) {
     city = input;
 }
-void setIATA(string input) {
+void Airport::setIATA(string input) {
     IATA = input;
 }
-void setLon(double input) {
+void Airport::setLon(double input) {
     lon = input;
 }
-void setLat(double input) {
+void Airport::setLat(double input) {
     lat = input;
 }
-void setAlt(double input) {
-    alt = input;
+// void Airport::setAlt(double input) {
+//     alt = input;
+// }
+
+// unordered_map<int, Airport> setMap(string input){
+//     std::ifstream aiportdata(filename);
+//     string line;
+//     unordered_map<int, Airport> ans;
+//     while (std::getline(airportdata, line)) {
+//         Aiport temp = setMapHelper(line);
+//         ans[Airport.]
+        
+//     }
+// }  
+
+vector<Airport> setVector(string input) {
+    std::ifstream aiportdata(filename);
+    string line;
+    vector<Airport> ans;
+    ans.push_back(Airport());
+    while (std::getline(airportdata, line)) {
+        Aiport temp = setVectorHelper(line);
+        ans.push_back(temp);
+    }
+}
+
+Airport setVectorHelper(string line) {
+    int counter = 0;
+    char character;
+    string word = "";
+    Airport soln = new Airport();
+
+    for(unsigned i = 0; i < line.size(), i++) {
+        character = line[i];
+        if (character = ",") {
+            if (counter = 1) {
+                soln.name = word;
+                word = "";
+            } else if (counter = 2){
+                soln.city = word;
+                word = "";
+            } else if (counter = 4) {
+                soln.IATA = word;
+                word = "";
+            } else of (counter = 6) {
+                soln.lon = word;
+                word = "";
+            } else if (counter = 7) {
+                soln.lat = word;
+                word = "";
+            } else {
+                word = "";
+            }
+
+            counter++;
+        } else if (character = '"') {
+            i++;
+        } else {
+            word += character;
+        }
+    }
 }
