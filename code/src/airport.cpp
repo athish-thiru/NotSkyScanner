3141e3a237fb426091a0e37c217107619a00970b
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-11-27 17:49:06 -0600

    Cleaned Airport and added a print function

--- code/src/airport.cpp
+++ code/src/airport.cpp
@@ -1,16 +1,16 @@
 #include "airport.h"
-#include <vector>
+#include "vector"
+#include <iostream>
 
 using namespace std;
 
 Airport::Airport() {
-    /*
     name = "UNKNOWN";
     city = "UNKNOWN";
     IATA = "UNKNOWN";
     lon = 0;
     lat = 0;
-    */
+
 }
 
 string Airport:: getName() {
@@ -28,9 +28,6 @@ double Airport:: getLon() {
 string Airport::getIATA() {
     return IATA;
 }
-// double Airport::getAlt() {
-//     return alt;
-// }
 
 void Airport::setName(string input) {
     name = input;
@@ -47,67 +44,7 @@ void Airport::setLon(double input) {
 void Airport::setLat(double input) {
     lat = input;
 }
-// void Airport::setAlt(double input) {
-//     alt = input;
-// }
 
-// unordered_map<int, Airport> setMap(string input){
-//     std::ifstream aiportdata(filename);
-//     string line;
-//     unordered_map<int, Airport> ans;
-//     while (std::getline(airportdata, line)) {
-//         Aiport temp = setMapHelper(line);
-//         ans[Airport.]
-        
-//     }
-// }  
-/*
-
-vector<Airport> setVector(string input) {
-    std::ifstream aiportdata(filename);
-    string line;
-    vector<Airport> ans;
-    ans.push_back(Airport());
-    while (std::getline(airportdata, line)) {
-        Aiport temp = setVectorHelper(line);
-        ans.push_back(temp);
-    }
+void Airport::printAirport() {
+    cout << "The Current Airport is " << name << " located in " << city << " which is latitude = " << lat << " and longitude = " << lon << " The IATA Code is " << IATA << endl;
 }
-
-Airport setVectorHelper(string line) {
-    int counter = 0;
-    char character;
-    string word = "";
-    Airport soln = new Airport();
-
-    for(unsigned i = 0; i < line.size(), i++) {
-        character = line[i];
-        if (character = ",") {
-            if (counter = 1) {
-                soln.name = word;
-                word = "";
-            } else if (counter = 2){
-                soln.city = word;
-                word = "";
-            } else if (counter = 4) {
-                soln.IATA = word;
-                word = "";
-            } else of (counter = 6) {
-                soln.lon = word;
-                word = "";
-            } else if (counter = 7) {
-                soln.lat = word;
-                word = "";
-            } else {
-                word = "";
-            }
-
-            counter++;
-        } else if (character = '"') {
-            i++;
-        } else {
-            word += character;
-        }
-    }
-}
-*/
