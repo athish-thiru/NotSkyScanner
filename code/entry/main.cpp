80a47abd5d7eda671448f238bc9d8ca4a5278b02
Sanjivani-Sharma <106048879+Sanjivani-Sharma@users.noreply.github.com>
2022-11-28 17:45:06 -0600

    print statements for graph tests added

--- code/entry/main.cpp
+++ code/entry/main.cpp
@@ -18,5 +18,28 @@ int main() {
     test_graph.addAllEdges();
     test_graph.printGraph(2912);
 
+
+
+    Routes routes;
+    Graph testgraph;
+
+//test to see value stored correctly in vertex of graph
+    testgraph.addEdge(routes.GetSourceNumbers()[0],routes.GetDestinationNumbers()[0],routes.GetDistances()[0]);
+
+    testgraph.printGraph(routes.GetSourceNumbers()[0]);
+
+    //test for skipping value with \N
+testgraph.addEdge(routes.GetSourceNumbers()[7],routes.GetDestinationNumbers()[7],routes.GetDistances()[7]);
+  testgraph.printGraph(routes.GetSourceNumbers()[7]);
+    cout<<"previous value skipped due to \\N"<<endl;
+
+
+    //test for multiple edges
+    testgraph.addEdge(routes.GetSourceNumbers()[12],routes.GetDestinationNumbers()[12],routes.GetDistances()[12]);
+     testgraph.addEdge(routes.GetSourceNumbers()[13],routes.GetDestinationNumbers()[13],routes.GetDistances()[13]);
+      testgraph.addEdge(routes.GetSourceNumbers()[14],routes.GetDestinationNumbers()[14],routes.GetDistances()[17]);
+      testgraph.printGraph(routes.GetSourceNumbers()[12]);
+
+
     return 0;
 } 
