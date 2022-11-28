1451cac85700c123baf828a10c2664038cd00725
Jasmehar-Kochhar <90488944+Jasmehar-Kochhar@users.noreply.github.com>
2022-11-28 17:45:53 -0600

    Update testParsing.cpp

--- tests/testParsing.cpp
+++ tests/testParsing.cpp
@@ -66,12 +66,16 @@ TEST_CASE("Skips /N values", "[Routes = 1]") {
     vector<int> ans_source_id;
     vector<int> ans_destination_id;
     vector<double> ans_distance;
+    vector<double> test_distance; 
 
     Routes routes;
 
     vector<int> test_source_id = {2965,2966,2966,2968,2968,4029,4029,4029,6156,6156};
     vector<int> test_destination_id = {2990,2990,2962,2990,4078,2990,6969,6160,2952,2990};
-    vector<double> test_distance = {4418.96,12717.7,4287.81,436.354,5265.3,18702.8,18664.9,18702.8,3407.11,4220.92};
+    for(int i = 0; i< 10;i++ ){
+        test_distance.push_back(routes.distance(test_source_id[i],test_destination_id[i]));
+    }
+    //{ 1410.3985310865, 698.9271709004, 570.2167546593, 1358.753027497, 2350.3060694337, 1264.0110818466, 1576.6803538014, 1656.5176917083, 1825.0159036461, 1466.0666625335 };
 
     for(int i; i<10; i++){
         ans_source_id.push_back(routes.GetSourceNumbers()[i]);
@@ -86,7 +90,7 @@ TEST_CASE("Skips /N values", "[Routes = 1]") {
 
     REQUIRE(test_source_id == ans_source_id);
     REQUIRE(test_destination_id == ans_destination_id);
-    REQUIRE(test_distance == test_distance);
+    REQUIRE(test_distance == ans_distance);
 }
 
 TEST_CASE("No extra values in any vector/ all the data has been parsed", "[Routes = 2]") {
