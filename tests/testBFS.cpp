48352ab8050fac1715562e0f343f59f44d0c82a7
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-12-04 17:18:07 -0600

    Update testBFS.cpp

--- tests/testBFS.cpp
+++ tests/testBFS.cpp
@@ -1,5 +1,19 @@
 #include <catch2/catch_test_macros.hpp> 
+#include <iostream>
+#include "../code/src/airport.h"
+#include "../code/src/graph.h"
+#include "../code/src/routes.h"
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <bits/stdc++.h>
 
-TEST_CASE("Random Test Case 1", "[BFS]") {
-    REQUIRE(1 == 1);
+using namespace std;
+
+TEST_CASE("BFS Test Case 1", "[BFS]") {
+    Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
+    test_graph.addAllEdges();
+    vector<string> temp = test_graph.BFS(100);
+
+    REQUIRE(temp == <"Arya Airport", "Athish Airport", "Mount Hagen Kagamuga Airport", "Nadzab Airport">);
 }
