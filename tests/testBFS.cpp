12c86672f7a3612024771ecc66a6f9f8d9ad7d1e
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-12-05 19:15:02 -0600

    More tests!

--- tests/testBFS.cpp
+++ tests/testBFS.cpp
@@ -18,3 +18,39 @@ TEST_CASE("BFS Test Case 1", "[BFS]") {
 
     REQUIRE(temp == expected);
 }
+
+TEST_CASE("BFS Test Case 2", "[BFS]") {
+    Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
+    test_graph.addAllEdges();
+    vector<string> temp = test_graph.BFS(3500);
+    std::vector<std::string> expected = {"Nadzab Airport"};
+
+    REQUIRE(temp == expected);
+}
+
+TEST_CASE("BFS Test Case 3", "[BFS]") {
+    Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
+    test_graph.addAllEdges();
+    vector<string> temp = test_graph.BFS(45);
+    std::vector<std::string> expected = {};
+
+    REQUIRE(temp == expected);
+}
+
+TEST_CASE("BFS Test Case 4", "[BFS]") {
+    Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
+    test_graph.addAllEdges();
+    vector<string> temp = test_graph.BFS(42);
+    std::vector<std::string> expected = {"Lonely Airport"};
+
+    REQUIRE(temp == expected);
+}
+
+TEST_CASE("BFS Test Case 5", "[BFS]") {
+    Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
+    test_graph.addAllEdges();
+    vector<string> temp = test_graph.BFS(250);
+    std::vector<std::string> expected = {"Athish Airport", "Mount Hagen Kagamuga Airport", "Nadzab Airport"};
+
+    REQUIRE(temp == expected);
+}
