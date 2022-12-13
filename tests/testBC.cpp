0a8092f8d47c6e2d26120637bc4edd128db702ea
aharia2 <90649056+aharia2@users.noreply.github.com>
2022-12-12 23:22:52 -0600

    Update testBC.cpp

--- tests/testBC.cpp
+++ tests/testBC.cpp
@@ -1,46 +1,50 @@
 #include <catch2/catch_test_macros.hpp> 
 #include "../code/src/Graph.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include <algorithm>
+
+#include <bits/stdc++.h>
+
 
 using namespace std;
 
 TEST_CASE ("Betweenness Centrality Score test case 1", "[betweennessCentrality]"){
 Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
 test_graph.addAllEdges();
-vector<float> BCtest = testGraph.betweennessCentrality();
-vector<float> expected_val = {};
-  
+vector<float> BCtest = test_graph.betweennessCentrality(5);
+vector<float> expected_val;
+
+for (int i = 0; i < 14111; i++) {
+    expected_val.push_back(0);
+}
+
 REQUIRE (BCtest == expected_val); 
 
 }
 
-TEST_CASE ("Betweenness Centrality test case 2", "[betweennessCentrality]"){
+TEST_CASE ("Betweenness Centrality Score test case 2", "[betweennessCentrality]"){
 Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
 test_graph.addAllEdges();
-vector<float> BCtest = testGraph.betweennessCentrality();
-vector<float> expected_val = {};
-  
+vector<float> BCtest = test_graph.betweennessCentrality(5);
+vector<float> expected_val(14111);
+
 REQUIRE (BCtest == expected_val); 
 
 }
 
 TEST_CASE ("Betweenness Centrality Score test case 3", "[betweennessCentrality]"){
-Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
+Graph test_graph = Graph("../data/airports.dat", "../data/routes.dat");
 test_graph.addAllEdges();
-vector<float> BCtest = testGraph.betweennessCentrality();
-vector<float> expected_val = {};
-  
-REQUIRE (BCtest == expected_val); 
+vector<float> BCtest = test_graph.betweennessCentrality(5);
+vector<float> expected_val = {0.1, 0.1, 0.2, 0.3, 0.9};
 
+REQUIRE (BCtest[0] == expected_val[0]); 
+REQUIRE (BCtest[1] == expected_val[1]); 
+REQUIRE (BCtest[2] == expected_val[2]); 
+REQUIRE (BCtest[3] == expected_val[3]); 
+REQUIRE (BCtest[4] == expected_val[4]); 
 }
-
-
-TEST_CASE ("Betweenness Centrality Score test case 4", "[betweennessCentrality]"){
-Graph test_graph = Graph("../data/testAirports.dat", "../data/testRoutes.dat");
-test_graph.addAllEdges();
-vector<float> BCtest = testGraph.betweennessCentrality();
-vector<float> expected_val = {};
-  
-REQUIRE (BCtest == expected_val); 
-
-}
-
